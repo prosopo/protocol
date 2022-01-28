f6ccbbc6abb97b8a779cf82762e8c1f7998355c3
Sinisa Canak <sinisa.canak997@gmail.com>
2022-01-28 14:32:09 +0100

    add service_origin used check

--- scripts/types.ts
+++ scripts/types.ts
@@ -34,6 +34,7 @@ const definitions = {
             'ProviderDoesNotExist',
             'ProviderInsufficientFunds',
             'ProviderInactive',
+            'ProviderServiceOriginUsed',
             'DuplicateCaptchaDataId',
             'DappExists',
             'DappDoesNotExist',
