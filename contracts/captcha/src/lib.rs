aee9618fd0b467383c0880c08f21dd5f7fabf45c
George Oastler <goastler4@gmail.com>
2023-05-15 17:31:36 +0100

    fix tests

--- contracts/captcha/src/lib.rs
+++ contracts/captcha/src/lib.rs
@@ -1655,7 +1655,7 @@ pub mod prosopo {
                 // only able to instantiate from the alice account
                 set_caller(default_accounts().alice);
                 let contract =
-                    Prosopo::default(STAKE_THRESHOLD, STAKE_THRESHOLD, 10, 1000000, 0, 1000);
+                    Prosopo::new(STAKE_THRESHOLD, STAKE_THRESHOLD, 10, 1000000, 0, 1000);
                 // should construct successfully
             }
 
@@ -1668,7 +1668,7 @@ pub mod prosopo {
                 // only able to instantiate from the alice account
                 set_caller(default_accounts().bob);
                 let contract =
-                    Prosopo::default(STAKE_THRESHOLD, STAKE_THRESHOLD, 10, 1000000, 0, 1000);
+                    Prosopo::new(STAKE_THRESHOLD, STAKE_THRESHOLD, 10, 1000000, 0, 1000);
                 // should fail to construct and panic
             }
 
