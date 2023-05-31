b1a66b921d801192e9903c925c47e72592f0dedc
George Oastler <goastler4@gmail.com>
2023-05-31 12:07:09 +0100

    skip common_dev contract when building (does not compile due to being dev/testing module)

--- dev/src/cli.ts
+++ dev/src/cli.ts
@@ -261,9 +261,9 @@ export async function processArgs(args: string[]) {
                 const contracts = argv.package as string[];
                 delete argv.package;
                 for(const contract of contracts) {
-                    if(contract === "common") {
-                        // skip common contract as it is not a proper contract, only used for library purposes and does not build independently
-                        console.log("Skipping common contract");
+                    if(contract === "common_dev") {
+                        // skip common_dev contract as it is not a proper contract, only used for library purposes and does not build independently
+                        console.log("Skipping common_dev contract");
                     }
                     await execCargo(argv, 'contract build', `${contractsDir}/${contract}`)
                 }
